f6a61d591676b639d81650b2e3ded780c6e328d4
Kaarel Ratas <kaarel.ratas@gmail.com>
2016-01-06 17:51:02 +0200

    Added new types and (de)serializing behavior.

--- serdepa/serdepa.py
+++ serdepa/serdepa.py
@@ -4,23 +4,47 @@ __author__ = "Raido Pahtma, ..."
 __license__ = "MIT"
 
 
+import StringIO
+import numbers
+import struct
+import collections
+import warnings
+import copy
+
+
 # TODO probably should define custom types to represent different variable types
 # ... or maybe use ctypes types like in the initial tests ...?
 
-def add_property(cls, attr, default):
+def add_property(cls, attr, attr_type):
     if hasattr(cls, attr):
-        print "NOT adding property %s to %s" % (attr, cls)  # In the final version this should probably raise an exception once metaclasses are handled properly
+#        print "NOT adding property %s to %s" % (attr, cls)  # In the final version this should probably raise an exception once metaclasses are handled properly
+        pass
     else:
-        print "adding property %s to %s" % (attr, cls)
+#        print "adding property %s to %s" % (attr, cls)
 
-        def setter(self, v):
-            print "setter for %s" % (attr)  # TODO remove this debug
-            setattr(self, '_%s' % attr, v)
+        if isinstance(attr_type, BaseIterable):
+            setter = None
+            def getter(self):
+                return getattr(self, '_%s' % attr)
 
-        def getter(self):
-            return getattr(self, '_%s' % attr, default)
+        elif isinstance(attr_type, Length):
+            setter = None
+            def getter(self):
+                return len(getattr(
+                    self,
+                    '_field_regitry'
+                )[getattr(self, '_depends')[attr]])
+
+        else:
+            def setter(self, v):
+#                print "setter for %s" % (attr)  # TODO remove this debug
+                setattr(getattr(self, '_%s' % attr), "value", v)
+
+            def getter(self):
+                return getattr(self, '_%s' % attr).value
 
         setattr(cls, attr, property(getter, setter))
+#        print(dir(cls))
 
 
 class SuperSerdepaPacket(type):
@@ -28,13 +52,22 @@ class SuperSerdepaPacket(type):
     def __init__(cls, what, bases=None, attrs=None):
         print "SuperTransformPacket cls:%s what:%s bases:%s attrs:%s" % (cls, what, bases, attrs)  # TODO remove
 
+        # TODO only allow the last member to be of unknown length
+        setattr(cls, "_fields", collections.OrderedDict())
+        setattr(cls, "_depends", dict())
         if '_fields_' in attrs:
             for field in attrs['_fields_']:
                 if len(field) == 2:
-                    if isinstance(field[1], List):
-                        setattr(cls, field[0], [])
-                    else:
-                        add_property(cls, field[0], "")
+                    add_property(cls, field[0], field[1])
+                    getattr(cls, "_fields")[field[0]] = field[1]
+                    if isinstance(field[1], Length):
+                        getattr(cls, "_depends")[field[0]] = field[1]._field
+                    elif isinstance(field[1], List):
+                        if not (field[0] in getattr(cls, "_depends").values() or field == attrs['_fields_'][-1]):
+                            raise TypeError("Only the last field can have an undefined length ({} of type {})".format(
+                                field[0],
+                                type(field[1])
+                            ))
                 else:
                     raise TypeError("A field needs both a name and a type")
 
@@ -46,30 +79,372 @@ class SuperSerdepaPacket(type):
 class SerdepaPacket(object):
     __metaclass__ = SuperSerdepaPacket
 
+    def __init__(self, **kwargs):
+        self._field_regitry = collections.OrderedDict()
+#        for field in kwargs:
+#            if field not in self._field_regitry:
+#                raise TypeError("Field {field} not in {cls}.".format(
+#                    field=field,
+#                    cls=self.__class__.__name__
+#                ))
+        for name, _type in self._fields.iteritems():
+            if name in kwargs:
+                self._field_regitry[name] = _type(initial=copy.copy(kwargs[name]))
+            else:
+                self._field_regitry[name] = _type()
+            setattr(self, '_%s' % name, self._field_regitry[name])
+
+
     def serialize(self):
         # TODO loop over _fields_ and serialize them
-        return ""
+        serialized = StringIO.StringIO()
+        for name, field in self._field_regitry.iteritems():
+            if name in self._depends:
+                serialized.write(
+                    field.serialize(self._field_regitry[self._depends[name]].length)
+                )
+            else:
+                serialized.write(field.serialize())
+        ret = serialized.getvalue()
+        serialized.close()
+        return ret
 
-    def deserialize(self, data):
+    def deserialize(self, data, pos=0):
         # TODO loop over _fields_ and deserialize their values from data
-        pass
+        for name, field in self._field_regitry.iteritems():
+            if pos == len(data):
+                raise AttributeError("Invalid length of data to deserialize.")
+            try:
+                pos = field.deserialize(data, pos)
+            except IOError:
+                length = 0
+                for key, value in self._depends.iteritems():
+                    if name == value:
+                        pos = field.deserialize(data, pos, self._field_regitry[key]._type.value)
+                        break
+                else:
+                    pos = field.deserialize(data, pos, -1)
+            if pos > len(data):
+                raise AttributeError("Invalid length of data to deserialize.")
+#        else:
+#            if pos != len(data):
+#                warnings.warn(RuntimeWarning("Data longer than available fields"))
+        return pos
 
 
-class Length(object):
+class BaseField(object):
+
+    def __call__(self, **kwargs):
+        ret = copy.copy(self)
+        if "initial" in kwargs:
+            ret._set_to(kwargs["initial"])
+        return ret
+
+#    def __call__(self):
+#        return NotImplemented
+#    
+    def serialize(self):
+        return bytearray([])
+
+    def deserialize(self, value, pos):
+        return NotImplemented
+
+
+class BaseIterable(BaseField, list):
+
+    def __init__(self, initial=[]):
+        for value in initial:
+            self.append(self._type(initial=copy.copy(value)))
+
+    def _set_to(self, values):
+        while len(self) > 0:
+            self.pop()
+        for value in values:
+            self.append(value)
+
+    def append(self, value):
+        if isinstance(value, self._type):
+            new_value = value
+        else:
+            new_value = self._type(initial=value)
+        super(BaseIterable, self).append(new_value)
+
+    def serialize(self):
+        ret = bytearray()
+        for i in xrange(self.length):
+            ret += self[i].serialize()
+        return ret
+
+    def deserialize(self, value, pos):
+        for i in xrange(self.length):
+            self[i] = self._type()
+            pos = self[i].deserialize(value, pos)
+        return pos
+
+    def __iter__(self):
+        for i in xrange(len(self)):
+            yield self[i].value
+
+
+class BaseInt(BaseField):
+    """
+    Base class for all integer types. Has the fields of
+    _length (in bits), _endianness ("B" for big, "L" for little)
+    and _signed (bool).
+    """
+
+    _endianness = None
+    _length = None
+    _signed = None
+    _format = ""
+
+    def __init__(self, initial=0):
+        self._value = initial
+
+    def _set_to(self, value):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = int(value)
+
+    def serialize(self):
+        return struct.pack(self._format, self._value)
+
+    def deserialize(self, value, pos):
+        self._value = struct.unpack(self._format, value[pos:pos+self.serialized_size()])[0]
+        return pos + self.serialized_size()
+
+    def serialized_size(self):
+        """
+        Returns the length of this field in bytes. If the length in bits is not directly
+        divisible by 8, an extra byte is added.
+        """
+
+        return self._length // 8 + bool(self._length % 8)
+
+    def __getattribute__(self, attr):
+        if attr in ["__lt__", "__le__", "__eq__", "__ne__", "__gt__", "__ge__",
+                "__add__", "__sub__", "__mul__", "__floordiv__", "__mod__",
+                "__divmod__", "__pow__", "__lshift__", "__rshift__", "__and__",
+                "__xor__", "__or__", "__div__", "__truediv__", "__str__"]:
+            return self._value.__getattribute__(attr)
+        else:
+            return super(BaseInt, self).__getattribute__(attr)
+
+    def __int__(self):
+        return self._value
+
+    def __repr__(self):
+        return "{} with value {}".format(self.__class__, self._value)
+
+
+class Length(BaseField):
+    """
+    A value that defines another field's length.
+    """
 
     def __init__(self, object_type, field_name):
-        self._type = object_type
+        self._type = object_type()
         self._field = field_name
 
+    def serialized_size(self):
+        return self._type.serialized_size()
 
-class List(object):
+    def serialize(self, length):
+        self._type.value = length
+        return self._type.serialize()
 
-    def __init__(self, object_type):
+    def deserialize(self, value, pos):
+        return self._type.deserialize(value, pos)
+
+
+class List(BaseIterable):
+    """
+    An array with its length defined elsewhere.
+    """
+
+    def __init__(self, object_type, **kwargs):
         self._type = object_type
+        super(List, self).__init__(**kwargs)
 
+    @property
+    def length(self):
+        return len(self)
 
-class Array(object):
+#    @length.setter
+#    def length(self, value):
+#        if isinstance(value, Length):
+#            self._length_field = value
+#        else:
+#            raise TypeError("The length attribute must be of type serdepa.Length, not {}".format(
+#                type(value)
+#            ))
 
-    def __init__(self, object_type, length):
+    def serialized_size(self):
+        return self._type.serialized_size() * self.length
+
+    def deserialize(self, value, pos, length=None):
+        if length is None:
+            raise IOError("Unknown length.")
+        elif length == -1:
+            for i in xrange(len(self), (len(value)-pos)/self._type().serialized_size()):
+                self.append(0)
+            return super(List, self).deserialize(value, pos)
+        else:
+            for i in xrange(len(self), length):
+                self.append(0)
+            return super(List, self).deserialize(value, pos)
+
+#    @property
+#    def value(self):
+#        return self._list
+
+class Array(BaseIterable):
+    """
+    A fixed-length array of values.
+    """
+
+    def __init__(self, object_type, length, **kwargs):
         self._type = object_type
         self._length = length
+        super(Array, self).__init__(**kwargs)
+        
+    @property
+    def length(self):
+        return self._length
+
+    def serialized_size(self):
+        return self._type.serialized_size() * self.length
+
+    def serialize(self):
+        dl = self.length - len(self)
+        if dl < 0:
+            warnings.warn(RuntimeWarning("The number of items in the Array exceeds the length of the array."))
+        elif dl > 0:
+            self += [self._type() for i in xrange(dl)]
+        ret = super(Array, self).serialize()
+        for i in xrange(dl):
+            self.pop(-1)
+        return ret
+
+    def deserialize(self, value, pos):
+        for i in xrange(len(self), self.length):
+            self.append(self._type())
+        return super(Array, self).deserialize(value, pos)
+
+
+class nx_uint8(BaseInt):
+    _signed = False
+    _length = 8
+    _endianness = "B"
+    _format = ">B"
+
+
+class nx_int8(BaseInt):
+    _signed = True
+    _length = 8
+    _endianness = "B"
+    _format = ">b"
+
+
+class uint8(BaseInt):
+    _signed = False
+    _length = 8
+    _endianness = "L"
+    _format = "<B"
+
+
+class int8(BaseInt):
+    _signed = True
+    _length = 8
+    _endianness = "L"
+    _format = "<b"
+
+
+class nx_uint16(BaseInt):
+    _signed = False
+    _length = 16
+    _endianness = "B"
+    _format = ">H"
+
+
+class nx_int16(BaseInt):
+    _signed = True
+    _length = 16
+    _endianness = "B"
+    _format = ">h"
+
+
+class uint16(BaseInt):
+    _signed = False
+    _length = 16
+    _endianness = "L"
+    _format = "<H"
+
+
+class int16(BaseInt):
+    _signed = True
+    _length = 16
+    _endianness = "L"
+    _format = "<h"
+
+
+class nx_uint32(BaseInt):
+    _signed = False
+    _length = 32
+    _endianness = "B"
+    _format = ">I"
+
+
+class nx_int32(BaseInt):
+    _signed = True
+    _length = 32
+    _endianness = "B"
+    _format = ">i"
+
+
+class uint32(BaseInt):
+    _signed = False
+    _length = 32
+    _endianness = "L"
+    _format = "<I"
+
+
+class int32(BaseInt):
+    _signed = True
+    _length = 32
+    _endianness = "L"
+    _format = "<i"
+
+
+class nx_uint64(BaseInt):
+    _signed = False
+    _length = 64
+    _endianness = "B"
+    _format = ">L"
+
+
+class nx_int64(BaseInt):
+    _signed = True
+    _length = 64
+    _endianness = "B"
+    _format = ">l"
+
+
+class uint64(BaseInt):
+    _signed = False
+    _length = 64
+    _endianness = "L"
+    _format = "<L"
+
+
+class int64(BaseInt):
+    _signed = True
+    _length = 64
+    _endianness = "L"
+    _format = "<l"
--- serdepa/tests/test_serdepa.py
+++ serdepa/tests/test_serdepa.py
@@ -2,46 +2,86 @@
 
 import unittest
 
-from serdepa import SerdepaPacket, Length, List, Array
+from serdepa import SerdepaPacket, Length, List, Array, \
+        nx_uint8, nx_uint16, nx_uint32, nx_uint64, \
+        nx_int8, nx_int16, nx_int32, nx_int64, \
+        uint8, uint16, uint32, uint64
 
-from ctypes import c_uint8, c_uint32, c_int32
 
-
-__author__ = "Raido Pahtma, ..."
+__author__ = "Raido Pahtma, Kaarel Ratas, ..."
 __license__ = "MIT"
 
 
 class PointStruct(SerdepaPacket):
     _fields_ = [
-        ("x", c_int32),
-        ("y", c_int32)
+        ("x", nx_int32),
+        ("y", nx_int32)
     ]
 
 
 class OnePacket(SerdepaPacket):
     _fields_ = [
-        ("header", c_uint8),
-        ("timestamp", c_uint32),
-        ("length", Length(c_uint8, "data")),
-        ("data", List(c_uint8)),
-        ("tail", List(c_uint8))
+        ("header", nx_uint8),
+        ("timestamp", nx_uint32),
+        ("length", Length(nx_uint8, "data")),
+        ("data", List(nx_uint8)),
+        ("tail", List(nx_uint8))
     ]
 
 
 class AnotherPacket(SerdepaPacket):
     _fields_ = [
-        ("header", c_uint8),
-        ("timestamp", c_uint32),
+        ("header", nx_uint8),
+        ("timestamp", nx_uint32),
         ("origin", PointStruct),
-        ("points", Length(c_uint8, "data")),
+        ("points", Length(nx_uint8, "data")),
         ("data", List(PointStruct))
     ]
 
 
 class ArrayPacket(SerdepaPacket):
     _fields_ = [
-        ("header", c_uint8),
+        ("header", nx_uint8),
         ("data", Array(PointStruct, 4))
+    ]
+
+class SimpleArray(SerdepaPacket):
+    _fields_ = [
+        ("data", Array(nx_uint8, 10))
+    ]
+
+
+class MyNodes(SerdepaPacket):
+    _fields_ = [
+        ("nodeId", nx_uint16),
+        ("attr", nx_int16),
+        ("inQlty", nx_uint8),
+        ("outQlty", nx_uint8),
+        ("qlty", nx_uint8),
+        ("lifetime", nx_uint8)
+    ]
+
+
+class MyRouters(SerdepaPacket):
+    _fields_ = [
+        ("beatId", nx_uint32),
+        ("routerId", nx_uint16),
+        ("partnerId", nx_uint16),
+        ("attr", nx_uint16),
+        ("qlty", nx_uint8),
+        ("lifetime", nx_uint8),
+        ("flags", nx_uint8),
+    ]
+
+
+class BeatRecord(SerdepaPacket):
+    _fields_ = [
+        ("clockstamp", nx_uint32),
+        ("nodes_in_beat", Length(nx_uint8, "nodes")),
+        ("beats_in_cycle", Length(nx_uint8, "routers")),
+        ("my_beat_id", nx_uint32),
+        ("nodes", List(MyNodes)),
+        ("routers", List(MyRouters))
     ]
 
 
@@ -70,9 +110,48 @@ class TransformTester(unittest.TestCase):
         self.assertEqual(p.length, 4)
         self.assertEqual(len(p.data), 4)
         self.assertEqual(len(p.tail), 2)
-        self.assertEqual(p.data, [1, 2, 3, 4])
-        self.assertEqual(p.tail, [5, 6])
+        self.assertEqual(list(p.data), [1, 2, 3, 4])
+        self.assertEqual(list(p.tail), [5, 6])
 
+
+class ArrayTester(unittest.TestCase):
+    a1 = "00010203040506070809"
+    a2 = "000000000100000002000000030000000400000005000000060000000000000000"
+
+    def test_simple_array(self):
+        p = SimpleArray()
+        for i in xrange(10):
+            p.data.append(i)
+        self.assertEqual(p.serialize(), self.a1.decode("hex"))
+
+    def test_single_array(self):
+        p = ArrayPacket()
+        p.header = 0
+        p.data.append(PointStruct(x=1, y=2))
+        p.data.append(PointStruct(x=3, y=4))
+        p.data.append(PointStruct(x=5, y=6))
+
+        self.assertEqual(p.serialize(), self.a2.decode("hex"))
+
+    def test_single_array_deserialize(self):
+        p = ArrayPacket()
+        p.deserialize(self.a2.decode("hex"))
+
+        self.assertEqual(p.header, 0)
+        self.assertEqual(len(p.data), 4)
+        self.assertEqual(p.data[1].x, 3)
+        self.assertEqual(p.data[3].y, 0)
+
+
+class TestHourlyReport(unittest.TestCase):
+    report = "1DD26640070D0005029E022B0139FFFF0003029E010EFFFF3D0300000000FFFF000000000000FFFF000000000000FFFF000000000000FFFF000000000000FFFF000000000000000000000000000000000201AB029E01AB000000150000030296029E02350000001500000000000000000000000000000000000000000000000000000000000000000000000000000000000701FA022B022D00FF381300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+
+    def test_hourly_deserialize(self):
+        r = BeatRecord()
+        r.deserialize(self.report.decode("hex"))
+
+        self.assertEqual(r.nodes_in_beat, 7)
+        self.assertEqual(r.beats_in_cycle, 13)
 
 if __name__ == '__main__':
     unittest.main()
